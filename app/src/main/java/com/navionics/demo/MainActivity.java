76874257eabfe8eabf36c7402f5738f6cbcde998
Marco Ciarcia <mciarcia@navionics.com>
2020-01-22 09:03:47 +0100

    Updated sample application for Navionics MobileSDK

--- app/src/main/java/com/navionics/demo/MainActivity.java
+++ app/src/main/java/com/navionics/demo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.navionics.demo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,26 +32,25 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    static List<NMSCircle> mCircles = new ArrayList<NMSCircle>();
-    static List<NMSPolygon> mPolygons = new ArrayList<NMSPolygon>();
-    static List<NMSMarker> mMarkers = new ArrayList<NMSMarker>();
-    static List<NMSPolyline>mPolylines = new ArrayList<NMSPolyline>();
-    static List<NMSGroundOverlay>mGroundOverlays = new ArrayList<NMSGroundOverlay>();
-    static boolean gpsEnabled=false;
-    static boolean downloadEnabled=false;
-    Button zoomIn;
-    Button zoomOut;
-    Button circle;
-    Button polygon;
-    Button polyline;
-    Button marker;
-    Button login;
-    Button download;
-    Button gps;
+    private static final List<NMSCircle> mCircles = new ArrayList<>();
+    private static final List<NMSPolygon> mPolygons = new ArrayList<>();
+    private static final List<NMSMarker> mMarkers = new ArrayList<>();
+    private static final List<NMSPolyline>mPolylines = new ArrayList<>();
+    private static final List<NMSGroundOverlay>mGroundOverlays = new ArrayList<>();
+    private static boolean gpsEnabled=false;
+    private static boolean downloadEnabled=false;
+    private Button zoomIn;
+    private Button zoomOut;
+    private Button circle;
+    private Button polygon;
+    private Button polyline;
+    private Button marker;
+    private Button login;
+    private Button download;
+    private Button gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         NMSSettings settings = NMSSettings.settings();
         settings.setLanguage(NMSEnum.NMSLanguage.NMSLanguageEnglish);
@@ -58,9 +59,24 @@ public class MainActivity extends AppCompatActivity {
         settings.setPrivateKey("-----BEGIN RSA PRIVATE KEY-----\nMIIJKQIBAAKCAgEA4hfcLtLO3Yls+2X0M3yrX0JwtTB4ZrY4a19aurQjnb+Ua9Qn\nOoND4/bRJgcyF2wpZsxXQ/nx1HLDr/vbhL6prNRiFcL7h7notJrEBgbRtYMYt1nx\n6Uap+yI1ihRENkqrspa6VS2A7UEJ800KATvu8vvxluFFNdtzhA9tsDHd5BLkf7EX\nb//J+yzFi8LFysEasD3QmyPtgcMOh6iS8T6F679ucminmNKkiSNlW8FTqjfBKlNN\nY1SHG+ChnChukncuqOvcs3d3n1KuqjaL+UrXxLzmRYcX1SlGQ46R5pUDK9Z8RSrl\nV49zmgD0P9zmNZJAqAR6woI1i8vBMFGfq9AE944fQiR0iXs77YSR/JH2H+dJC+rg\njWxG6CEKwusKDmxT287V4T6b9fQFgyO9pvl50abpIIn16Vwd81Xbwa176fNxd2bE\ndt2k12tmS+0ZydXNt+u0zKgKhQ/yTAxgTiHH08YqvHg22YVrwYvZ8wR/Y5dKzHqa\n4htVgoruqpnX9GBlWfueDj3zoguhUTZTeNFIs7CyoQSsIr2GiJ3nFb41/DH2OWxW\nr1HCrKcmZIJE+6ivUUqeXJwNCT1R8SVCcAl71Bj0pVRCD2c5exSSim8J2ZHCtU6C\n2q82BT0Re5GGJiOAdH/ltU+RWjPP+0vXlMg/4eQb2KKwcwyMoZQGmNWnQyECAwEA\nAQKCAgEA1F6jJJxBUZh/dNj8e8xT0KPA6IDB54CsGDc2Kv+AIPHOQUbDHlQkViIF\nrNm/dH7VmMjCA4joXaz7IPfK57KMAFpyU4yV5ZR8AAELtbl8DCo68iabc4o7qjgk\n4DFPLUwQYSzxk4atfq3D4fRwPF6GgVikQhwRfZhHtjkjyAffLBeO6F547Gvw1mzk\nlfX7AgtGvVsi8kbaFjQZFtYCSHZ5JsrmvLKPEp5xjJth3sEGjHHYkKA0pePFESt7\nwgm8avlmK6d45F6IT3BDvoCEFEL3z8W76n0Npt0hMd3eMB+yHeBXVEUJraiCZpsU\n0lP1LUbWUrv6b3ANSeW27mMNPz6jGf01WJKJNVvFq7HTACeNEiA1Vfeg84JcPfRe\nBsHtSUGrXbMAKNCL47gK+gvKfllT8vLDi0N6ApMziRw9mT33zCTXQEGcaM7ap6XI\n46kqznXTM7g+QNW4lT68/3R0vGez2LhSBMGUhB7mnoEvP3bp3vAS5wNEO3stQ0FV\nK7ftZABusJojPoVbPqSr4QrTMFBFsh/qxLhlIkmAbJmZ6eZrsAik4EyH8iiLQA3n\nA/KldpC8yINn+1KjpeesJexcRtIX+bxFEmmNE+esDJIHl/8yOuctfL7uSmkNVBbJ\niONH1VN/R1qfOuPomIzDIWtWjcg6jeM6y49sLEoP2pogV76lkl0CggEBAPO92kPA\nNU98oh4adc3Cozjqq7L2jj6VVUpBSaxJJRQB1FL4ofJ5/7RqDfXdi2QXSXKuYPrh\n5k6erMG0SZQLY1/o2aSIxDaV+abievRnY0gPbxFugbBHtd2ENSoG7WbwkkNnxB5/\njhNcUSYv8VYXQQhpW4OUgfBoV3JgZpikcFv3umcPiyC/9Q+7wqnRExF0lyKt31px\nlk0pt+fIR60o2r1cJHwsrUTCHaREutIMpD3mslpcfaYuV7atG6O1YJjP+OREWoyO\nGEdlbDowgeW1rPZZ4thf514u8Y+erxg9Lb3Z7VS7oJcn5RQSYsakEpPz7pFagJ8g\nH39CGtvDH8pMnqcCggEBAO12yT5rbcNYXN9orzcGDBb20k9hpdZ1oDoqWiSf9ChX\nBa6ABkAy3IT4UsWVYH0CkReYa8AT9ShtibU9xiADn6d8K0si1xB0x4AnRdlZ+kLD\n0lOAnOahQgChMIp6E91JG87gtUHPqDfh5XrA4WFzRKnDN1NskPegekLNIZRMmLlI\ni4c3bIaxwJrKezZppAKZZPqFeKF3lOpqNKFZn003NKRVtdVJNQu6WshZRxKLasyx\nTW7eTFqzITZkCUQk5QdwQGilqw2z7y/DrA1rwH1GtU+5qnVCR69c2P/AymqAmPfQ\nnA/YEiupZO7acX1jJR97PvLSUOOqBx2bWatsCEVqUPcCggEAbZ1G6EsrbmjNe55I\ntlU/FFytBNnO1KRR7Af6eumWLC46b2nzYtmsvlUnnBebFVNHq7RyVsF60oaXiLFs\nmZCZi0trfYwoOagu0vdtdjZ8tq6CM6Ov3TgSuOE5C4J8B1xPWtLypwiUO2676+GJ\ntj7U91RyXHVQDm7OTNi/qwvgn+uDv8+EsSDCs+WrwLUOzz3Qrj7lgIYuotsNIsa2\nPVBvNtOWcOWispZdwD9MiSQ4RMJYGT0ZIMAcoEGveWsbWv3En9uBoU5R6uHYzz57\nXHPcqhl4Y/iVU4znA8DIW60/we6cxgtvSCsf4Wv3Uf/9ft4nvuSljWpj39Y5v/U7\neb4FZwKCAQEA7A31J3OrfVDNodhytQPx6LIbhoXPUU1Epg2L5nSLRb6cC0eA7zig\nNh6USY2giSSQDyCZnCs6vaGGDwEFfrWbc+bfMqXDnLw5xY9ExDfJq6z128QyNKGA\n9xdFJaMNNCCE4DZIeM9wrXyFnm02nqTzUL6atPdrdH22i8lW5BDwkN8otJXH8G2q\nxTUbHpANVfgehVtA+2HgStag0vRAg4WpcuMxCoDnlmz96cO1/x1QYYKvMtMQm+kN\nzv/Kpk72zVos7NojHFAOKTEeS4kdpGsxubsbU807bXuiyzpe/Vgwt8hMGwPzrCuM\nBlRoFPkF4jGl9cRUXLycrjXMAbg20KpLKQKCAQB/gSBZfKGgFTyGgbaMK1IZNUNR\n/q0ezcjxJQ98RnAhItH5L/rzs5hF1CRJqb/H6HXRg1TPfgaIaXSEwXwBk6NW7sQH\nCEO/hkutFvDx51FTbcDv+IPegzpEmfp9rvtXBht2444oA90Fwt3e7eQMT2aewFJJ\nJYo1UO6g0XNhQf/Lztbirkj6nnVd7k5nRTEHVjV3qrINlCliMjNiykBaS49WaUyh\nYe1IF8m4jsjlQq8CElyYUCLlse4M6ps0RpL0DoW50+J+x4LSlYoxEqE/BXLd8IqP\ny06nO//uL28VVvn6yuWKzl8+Nwjew/0obzF31TsnNR6bqXizAfRvJfnAENG4\n-----END RSA PRIVATE KEY-----\n");
 
         NSError error = new NSError();
-        NavionicsMobileServices.initializeWithSettings(settings, error);
+        boolean initResult = NavionicsMobileServices.initializeWithSettings(settings, error);
+        if (!initResult) {
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Mobile SDK Initialization error");
+            alertDialog.setMessage("The mobile sdk cannot be initialized, please check the configuration");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            System.exit(0);
+                        }
+                    });
+            alertDialog.show();
+            return;
+        }
+        setContentView(R.layout.activity_main);
 
-        zoomIn = (Button) findViewById(R.id.zoomIn);
+        zoomIn = findViewById(R.id.zoomIn);
         zoomIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        zoomOut = (Button) findViewById(R.id.zoomOut);
+        zoomOut = findViewById(R.id.zoomOut);
         zoomOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        circle = (Button) findViewById(R.id.circle);
+        circle = findViewById(R.id.circle);
         circle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,12 +104,12 @@ public class MainActivity extends AppCompatActivity {
                     addCircle(mapView);
                 } else {
                     circle.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-                    removeCircle(mapView);
+                    removeCircle();
                 }
             }
         });
 
-        polygon= (Button) findViewById(R.id.polygon);
+        polygon= findViewById(R.id.polygon);
         polygon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,11 +119,11 @@ public class MainActivity extends AppCompatActivity {
                     addPolygon(mapView);
                 } else {
                     polygon.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-                    removePolygon(mapView);
+                    removePolygon();
                 }
             }
         });
-        polyline = (Button) findViewById(R.id.polyline);
+        polyline = findViewById(R.id.polyline);
         polyline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -118,11 +134,11 @@ public class MainActivity extends AppCompatActivity {
                     addPolyline(mapView);
                 } else {
                     polyline.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-                    removePolyline(mapView);
+                    removePolyline();
                 }
             }
         });
-        marker = (Button) findViewById(R.id.marker);
+        marker = findViewById(R.id.marker);
         marker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -132,11 +148,11 @@ public class MainActivity extends AppCompatActivity {
                     addMarker(mapView);
                 } else {
                     marker.setBackgroundColor(getResources().getColor(R.color.darker_gray));
-                    removeMarker(mapView);
+                    removeMarker();
                 }
             }
         });
-        login= (Button) findViewById(R.id.login);
+        login= findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -144,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        download=(Button)findViewById(R.id.download);
+        download= findViewById(R.id.download);
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -160,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        gps=(Button)findViewById(R.id.gps);
+        gps= findViewById(R.id.gps);
         gps.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -172,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     switch (mapView.getGpsMode()) {
                         case NMSGPSModeUnlinked:
-                        {
+                        case NMSGPSModeCourseUpUnlinked: {
                             mapView.setGpsMode(NMSEnum.NMSGPSMode.NMSGPSModeNorthUp);
                             break;
                         }
@@ -190,11 +206,6 @@ public class MainActivity extends AppCompatActivity {
                         case NMSGPSModeCourseUp:
                         {
                             mapView.setGpsMode(NMSEnum.NMSGPSMode.NMSGPSModeCourseUpUnlinked);
-                            break;
-                        }
-                        case NMSGPSModeCourseUpUnlinked:
-                        {
-                            mapView.setGpsMode(NMSEnum.NMSGPSMode.NMSGPSModeNorthUp);
                             break;
                         }
                         default:
@@ -239,9 +250,10 @@ public class MainActivity extends AppCompatActivity {
             download.setBackgroundColor(Color.RED);
         }
     }
-    NMSMapView getMapView(){
+    private NMSMapView getMapView(){
         //(NMSMapView) findViewById(R.id.mapView);
         NMSMapFragment fg=(NMSMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        assert fg != null;
         return fg.getMapView();//R.id.mapView);
     }
     private void addCircle(NMSMapView view){
@@ -255,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
         NMSLocationCoordinate2D circleLocation = new NMSLocationCoordinate2D(41.814092,-70.5326246);
         view.moveToLocation(circleLocation, 16.0f, true);
     }
-    private void removeCircle(NMSMapView view){
+    private void removeCircle(){
         for (NMSCircle myCircle :mCircles){
             myCircle.setMap(null);
         }
@@ -278,7 +290,7 @@ public class MainActivity extends AppCompatActivity {
         NMSLocationCoordinate2D polygonLocation = new NMSLocationCoordinate2D(41.5943782,-70.3538668);
         view.moveToLocation(polygonLocation, 12.0f, true);
     }
-    private void removePolygon(NMSMapView view){
+    private void removePolygon(){
 
         for (NMSPolygon polygon :mPolygons){
             polygon.setMap(null);
@@ -301,7 +313,7 @@ public class MainActivity extends AppCompatActivity {
         NMSCameraPosition polylineLocation = NMSCameraPosition.cameraWithTarget(new NMSLocationCoordinate2D(41.5698391,-70.0216346),11.0f);
         view.moveToCameraPosition(polylineLocation, true);
     }
-    private void removePolyline(NMSMapView view){
+    private void removePolyline(){
         for (NMSPolyline polyline :mPolylines){
             polyline.setMap(null);
         }
@@ -320,7 +332,7 @@ public class MainActivity extends AppCompatActivity {
         view.moveToLocation(markerLocation, 13.0f, true);
     }
 
-    private void removeMarker(NMSMapView view){
+    private void removeMarker(){
         for (NMSMarker marker :mMarkers){
             marker.setMap(null);
         }
